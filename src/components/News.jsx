f3259bdac79f6b5c953f968889351622b65c26ef
Ike Odira <odiraike8@gmail.com>
2022-09-19 17:29:46 +0100

    Create Crypto News

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,9 @@
-
+import {useState} from "react";
 import {Select, Typography, Row, Col, Avatar, Card} from "antd";
 import moment from "moment";
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import {useGetCryptosQuery} from "../services/cryptoApi";
 
 const {Text, Title} = Typography;
 const {Option} = Select;
@@ -10,8 +11,10 @@ const {Option} = Select;
 const demoImage = 'http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg';
 
 function News({simplified}){
-    const {data:cryptoNews} = useGetCryptoNewsQuery({newsCategory:'Cryptocurrency', count:simplified ? 6:12})
-  
+    const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
+    const {data:cryptoNews} = useGetCryptoNewsQuery({newsCategory, count:simplified ? 6:12})
+    const { data} = useGetCryptosQuery(100);
+
     
     if(!cryptoNews?.value) return 'Loading...';
 
@@ -24,11 +27,16 @@ function News({simplified}){
                         className="select-news"
                         placeholder="Select a Cryto"
                         optionFilterProp="children"
-                        onChange={(value)=> console.log(value)}
+                        onChange={(value)=> setNewsCategory(value)}
                         filterOption={(input, option)=> option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
                     >
+                        <Option value="Cryptocurrency">Cryptocurrency</Option>
+                        {data?.data?.coins.map((coin)=>(
+                            <Option value={coin.name}>{coin.name}</Option>
+                        ))}
 
                     </Select>
+
                 </Col>
             )}
             {cryptoNews.value.map((news, index)=>(
